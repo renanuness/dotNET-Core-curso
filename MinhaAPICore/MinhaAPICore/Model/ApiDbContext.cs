249210e75bea3053c7a0548644b983665075f7de
renan.silva <renan.silva@RCA295>
2020-07-09 09:34:46 -0300

    primeira api com crud funcionando

---
+++ MinhaAPICore/MinhaAPICore/Model/ApiDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MinhaAPICore.Model
+{
+    public class ApiDbContext : DbContext
+    {
+        public ApiDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+
+    }
+
+}
